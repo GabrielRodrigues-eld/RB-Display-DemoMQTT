1a0c6ebaab385b42ab5e01f149c73341391cac20
GabrielRodrigues-eld <gabriel.rodrigues@eldorado.org.br>
2026-07-17 10:00:13 -0300

    test2 - caddy

--- config.js
+++ config.js
@@ -10,7 +10,7 @@
     debug: true,
 
     mqtt: {
-      url: "wss://127.0.0.1:9443",
+      url: "wss://localhost:9443",
       protocolVersion: 4,
       clientIdPrefix: "eldorado-rb-display",
       clean: true,
